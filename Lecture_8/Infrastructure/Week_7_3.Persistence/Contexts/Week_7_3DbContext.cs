92174fc3319380a53dcfd7245e134ed396ff129e
tarikemir <81103446+tarikemir@users.noreply.github.com>
2023-12-03 00:29:51 +0300

    local files uploaded

---
+++ Lecture_8/Infrastructure/Week_7_3.Persistence/Contexts/Week_7_3DbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_7_3.Domain.Common;
+using Week_7_3.Domain.Entities;
+
+namespace Week_7_3.Persistence.Contexts
+{
+    public class Week_7_3DbContext : DbContext
+    {
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<TaxiDriver> TaxiDrivers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasOne(p => p.Account)
+                .WithOne(ba => ba.Owner)
+                .HasForeignKey<BankAccount>(ba => ba.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
